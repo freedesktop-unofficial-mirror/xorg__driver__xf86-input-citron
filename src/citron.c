0297c49fd3743e15900a05169ba36e6a0fb438c0
Peter Hutterer <peter.hutterer@redhat.com>
2008-08-14 14:55:59 +0930

    Fix build, XF86_VERSION_CURRENT doesn't exist anymore.

--- src/citron.c
+++ src/citron.c
@@ -305,7 +305,7 @@ static XF86ModuleVersionInfo VersionRec =
 	MODULEVENDORSTRING,			/* vendor specific string */
 	MODINFOSTRING1,				
 	MODINFOSTRING2,
-	XF86_VERSION_CURRENT,		/* Current XFree version */
+	XORG_VERSION_CURRENT,		/* Current Xorg version */
 	CITOUCH_VERSION_MAJOR,		/* Module-specific major version */
 	CITOUCH_VERSION_MINOR,		/* Module-specific minor version */
 	CITOUCH_VERSION_PATCH,		/* Module-specific patch level */
